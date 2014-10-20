7b15e45a8333d11b56d78773513ba1977bdc3cf6
Alexey Terekhov <alexey.terekhov@bk.ru>
2014-10-20 09:04:28 +0700

    Good version of test

--- Day1610_HW_SeleniumWebdriver/soundcloud_test.py
+++ Day1610_HW_SeleniumWebdriver/soundcloud_test.py
@@ -29,101 +29,34 @@ class TestSoundCloud(TestCase):
 
         for item in items:
             # Check title
-            assert item.find_element_by_class_name("soundTitle__title")
-            assert item.find_element_by_class_name("soundTitle__username")
+            assert item.find_element_by_class_name("soundTitle__title").is_displayed()
+            assert item.find_element_by_class_name("soundTitle__username").is_displayed()
 
             # Check for picture
-            assert item.find_element_by_class_name("sc-artwork")
+            assert item.find_element_by_class_name("sc-artwork").is_displayed()
 
             # Check for track buttons
-            assert item.find_element_by_class_name("sc-button-like")
-            assert item.find_element_by_class_name("sc-button-repost")
-            assert item.find_element_by_class_name("sc-button-addtoset")
-            assert item.find_element_by_class_name("sc-button-share")
+            assert item.find_element_by_class_name("sc-button-like").is_displayed()
+            assert item.find_element_by_class_name("sc-button-repost").is_displayed()
+            assert item.find_element_by_class_name("sc-button-addtoset").is_displayed()
+            assert item.find_element_by_class_name("sc-button-share").is_displayed()
 
             # Check waveform
-            assert item.find_element_by_class_name("sound__waveform")
+            assert item.find_element_by_class_name("sound__waveform").is_displayed()
 
             # Upload time
-            assert item.find_element_by_class_name("sound__uploadTime")
+            assert item.find_element_by_class_name("sound__uploadTime").is_displayed()
 
             # Tag
-            assert item.find_element_by_class_name("soundTitle__tag")
+            assert item.find_element_by_class_name("soundTitle__tag").is_displayed()
 
             # Counters
-            assert item.find_element_by_class_name("sc-ministats-plays")
-            assert item.find_element_by_class_name("sc-ministats-likes")
-            assert item.find_element_by_class_name("sc-ministats-reposts")
+            assert item.find_element_by_class_name("sc-ministats-plays").is_displayed()
+            assert item.find_element_by_class_name("sc-ministats-likes").is_displayed()
+            assert item.find_element_by_class_name("sc-ministats-reposts").is_displayed()
 
         # It's ok!
         d.close()
-
-    # def test_search_track_controls(self):
-    #     d = webdriver.Firefox()
-    #     d.get("https://soundcloud.com/")
-    #     # Request input
-    #     s = WebDriverWait(d, 10).until(
-    #         expected_conditions.presence_of_element_located((By.CLASS_NAME, "headerSearch__input"))
-    #     )
-    #     s.send_keys("song")
-    #     b = d.find_element_by_class_name("headerSearch__submit")
-    #     b.click()
-    #     # Go to Tracks
-    #     t = WebDriverWait(d, 10).until(
-    #         expected_conditions.presence_of_element_located((By.CLASS_NAME, "g-nav-item-sounds"))
-    #     )
-    #     t.click()
-    #     # Wait for tracks
-    #     try:
-    #         WebDriverWait(d, 10).until(
-    #             expected_conditions.presence_of_element_located((By.CLASS_NAME, "searchList__item"))
-    #         )
-    #     except:
-    #         self.fail("No items at response!")
-    #     # Check title
-    #     try:
-    #         song = d.find_element_by_class_name("soundTitle__title")
-    #         author = d.find_element_by_class_name("soundTitle__username")
-    #     except:
-    #         self.fail("There are no title!")
-    #     # Check for picture
-    #     try:
-    #         pict = d.find_element_by_class_name("sc-artwork")
-    #     except:
-    #         self.fail("There is no picture!")
-    #     # Check for track buttons
-    #     try:
-    #         like = d.find_element_by_class_name("sc-button-like")
-    #         repost = d.find_element_by_class_name("sc-button-repost")
-    #         add = d.find_element_by_class_name("sc-button-addtoset")
-    #         share = d.find_element_by_class_name("sc-button-share")
-    #     except:
-    #         self.fail("Track response contains not all of buttons")
-    #     # Check waveform
-    #     try:
-    #         wave = d.find_element_by_class_name("sound__waveform")
-    #     except:
-    #         self.fail("There is no waveform!")
-    #     # Upload time
-    #     try:
-    #         upload = d.find_element_by_class_name("sound__uploadTime")
-    #     except:
-    #         self.fail("There is no upload time!")
-    #     # Tag
-    #     try:
-    #         tag = d.find_element_by_class_name("soundTitle__tag")
-    #     except:
-    #         self.fail("There is no tag!")
-    #     # Counters
-    #     try:
-    #         plays = d.find_element_by_class_name("sc-ministats-plays")
-    #         likes = d.find_element_by_class_name("sc-ministats-likes")
-    #         reposts = d.find_element_by_class_name("sc-ministats-reposts")
-    #         comments = d.find_element_by_class_name("sc-ministats-comments")
-    #     except:
-    #         self.fail("There is no counters!")
-    #     # It's ok!
-    #     # d.close()
 
 if __name__ == '__main__':
     unittest.main()
