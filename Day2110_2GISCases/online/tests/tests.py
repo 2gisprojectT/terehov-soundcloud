de766f3a41048a294b463fb2ededfc634b97893f
Alexey Terekhov <alexey.terekhov@bk.ru>
2014-10-24 18:21:27 +0700

    setUp & tearDown

--- Day2110_2GISCases/online/tests/tests.py
+++ Day2110_2GISCases/online/tests/tests.py
@@ -4,13 +4,19 @@ import unittest
 from selenium import webdriver
 from Day2110_2GISCases.online.helpers.page import Page
 
+
 class SeleniumTest(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(10)
+        self.page = Page(self.driver)
+        self.page.open("http://2gis.ru")
+
+    def tearDown(self):
+        self.driver.close()
 
     def test_share(self):
-        driver = webdriver.Firefox()
-        driver.implicitly_wait(10)
-        page = Page(driver)
-        page.open("http://2gis.ru")
+        page = self.page
         page.search_bar.search(u'синема')
         before_count = page.search_result.count
         before_text = page.search_bar.text
@@ -23,7 +29,12 @@ class SeleniumTest(TestCase):
         after_text = page.search_bar.text
         self.assertEqual(before_count, after_count, "Wrong share link!")
         self.assertEqual(before_text, after_text, "Wrong share link!")
-        driver.close()
+
+    def test_transport(self):
+        page = self.page
+        page.search_bar.change_mode(page.search_bar.selectors['mode_transport'])
+        page.search_bar.search_transport(u"Площадь Маркса", u"Заельцовская")
+        self.assertTrue(page.route_result.is_displayed(), "There are no route results!")
 
 if __name__ == '__main__':
     unittest.main()
